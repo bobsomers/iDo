c03f61155472e2c9de4eef6ea26054fc1765a500
Bob Somers <bob@bobsomers.com>
2017-03-20 18:36:02 -0700

    Cycle colors.

--- firmware/prototype/prototype.ino
+++ firmware/prototype/prototype.ino
@@ -16,6 +16,7 @@ void paint(Color c) {
 
 Color color;
 int dir = 1;
+int which = 0;
 
 void setup() {
   pinMode(LED_R, OUTPUT);
@@ -28,12 +29,40 @@ void setup() {
 }
 
 void loop() {
-  color.red += dir;
-  if (color.red == 255)  {
-    dir = -1;
-  } else if (color.red == 0) {
-    dir = 1;
+  switch (which) {
+  case 0:
+    color.red += dir;
+    if (color.red == 255)  {
+      dir = -1;
+    } else if (color.red == 0) {
+      dir = 1;
+      which = 1;
+    }
+    break;
+  case 1:
+    color.green += dir;
+    if (color.green == 255)  {
+      dir = -1;
+    } else if (color.green == 0) {
+      dir = 1;
+      which = 2;
+    }
+    break;
+
+  case 2:
+    color.blue += dir;
+    if (color.blue == 255)  {
+      dir = -1;
+    } else if (color.blue == 0) {
+      dir = 1;
+      which = 0;
+    }
+    break;
+
+  default:
+    which = 0;
   }
+  
   paint(color);
   delay(10);
 }
