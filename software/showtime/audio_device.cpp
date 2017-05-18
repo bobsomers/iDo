41db83c663bde9d69a2d05199a19db4a8711b303
Chris Gibson <cgibson@mrvoxel.com>
2017-05-18 03:15:58 -0700

    Added ring buffer for audio. Added fftw to build process. Tried (and failed) to add FFT calls... seems to be causing a segfault when uncommented

--- software/showtime/audio_device.cpp
+++ software/showtime/audio_device.cpp
@@ -1,12 +1,22 @@
 #include "audio_device.h"
 
 #include <iostream>
+#include <cmath>
+#include <mutex>
 
-#define SAMPLE_RATE (44100)
+#include "fftw3.h"
+
+constexpr unsigned int kSampleRate = 8000;
+constexpr unsigned int kBufferSize = kSampleRate / 10;
+constexpr float PI = 3.1415927;
+
+std::mutex mtx;
 
 namespace showtime {
 
 PaStream* AudioDevice::stream = nullptr;
+
+boost::circular_buffer<float> AudioDevice::buffer(kBufferSize);
 
 void AudioDevice::initialize() {
     // Don't allow multiple initializations
@@ -25,10 +35,10 @@ void AudioDevice::initialize() {
 
     // Open up a default stream (stereo output)
     err = Pa_OpenDefaultStream(&stream,
-                               2, // Stereo input
+                               1, // Mono input
                                0, // No output channel
                                paFloat32,
-                               SAMPLE_RATE,
+                               kSampleRate,
                                paFramesPerBufferUnspecified,
                                &AudioDevice::streamCallback,
                                NULL);
@@ -68,13 +78,78 @@ void AudioDevice::terminate() {
 
 }
 
+void lowPassFilter(std::vector<float>& data) {
+    static float lastInput = 0;
+    for (float& input : data) {
+        input = ( input + lastInput ) / 2 ;
+    }
+}
+
+void applyWindow(std::vector<float>& data) {
+    const auto size = data.size();
+    int i = 0;
+    for (float& input : data) {
+        // Build Hann window component
+        const auto window = 0.5 * ( 1 - cos( 2 * PI * i / (size-1.0) ) );
+
+        // Apply window
+        input = input * window;
+        ++i;
+    }
+}
+
+std::vector<float> AudioDevice::process() {
+    if (buffer.size() != 800) {
+        return {};
+    }
+
+    std::lock_guard<std::mutex> lock(mtx);
+
+    // Pull data from circular buffer
+    std::vector<float> data;
+    data.insert(data.begin(), AudioDevice::buffer.begin(), AudioDevice::buffer.end());
+
+    // Low pass filter to reduce noise
+    lowPassFilter(data);
+
+    // Apply Hann window to reduce sidelobes
+    applyWindow(data);
+
+    // unsigned int N = data.size();
+
+    // // Apply FFT
+    // float* out = (float*)fftwf_malloc(sizeof(float) * N);
+    // fftwf_plan p;
+    // p = fftwf_plan_r2r_1d(N, &data.front(), out, FFTW_R2HC, FFTW_FORWARD);
+    // fftwf_execute(p);
+
+    // std::vector<float> output(N/2 + 1);
+
+    // output[0] = out[0]*out[0];  /* DC component */
+    // for (unsigned int k = 1; k < (N+1)/2; ++k)  /* (k < N/2 rounded up) */
+    //     output[k] = out[k]*out[k] + out[N-k]*out[N-k];
+    // if (N % 2 == 0) /* N is even */
+    //     output[N/2] = out[N/2]*out[N/2];  /* Nyquist freq. */
+
+    // fftwf_free(out);
+
+    return output;
+}
+
 int AudioDevice::streamCallback(const void* input,
                                 void* output,
                                 unsigned long frameCount,
                                 const PaStreamCallbackTimeInfo* timeInfo,
                                 PaStreamCallbackFlags statusFlags,
                                 void* userData) {
+    std::lock_guard<std::mutex> lock(mtx);
+
     // Do all the things
+    float* in = (float*)input;
+    for (unsigned long i = 0; i < frameCount; ++i) {
+        AudioDevice::buffer.push_back(*(in++));
+    }
+
     return 0;
 }
 
